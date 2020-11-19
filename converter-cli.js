719e087eb308c13491b78e1ae2349dcbee3c7ab7
pvogel2 <pvogel2@freenet.de>
2020-11-19 22:24:19 +0100

    fix (cli): parse command line parameters

--- converter-cli.js
+++ converter-cli.js
@@ -1,42 +1,18 @@
 const path = require('path');
-const CONVERTER = require('./lib.js/api/converter');
+const cmdline = require('./lib.js/cli/commandline');
+const converter = require('./lib.js/api/converter');
 
-const args = process.argv.slice(2);
-console.log(process.env.NODE_ENV);
-/**
- * konwn parameter names
- */
-let sourceFile = ''; // required
-let targetFile = ''; // optional
-let debug = null; // optional
-
-for (let i = 0; i  < args.length; i += 2) {
-  const key = args[i];
-  const value = args[i+1];
-
-  switch(key) {
-    case '-i':
-    case '--in':
-      sourceFile = value;
-      break;
-    case '-o':
-    case '--out':
-      targetFile = value;
-      break;
-    default: {
-      console.log(`unknown parameter ${key}, exiting`);
-      process.exit(1);
-    }
-  }
-}
+const sourceFile = cmdline.getSource(); // required
+const targetFile = cmdline.getTarget(); // optional
 
 if (!sourceFile) {
-  console.log('missing source or target');
+  console.error('missing source file, exiting');
   process.exit(1);
 }
+
 
 const extName = path.extname(sourceFile);
 const targetName = path.basename(targetFile) || `bmout_${path.basename(sourceFile, extName)}.png`;
 const targetPath = path.dirname(targetFile) || '';
 
-CONVERTER.calcHeightmap(sourceFile, path.join(targetPath, targetName));
+converter.calcHeightmap(sourceFile, path.join(targetPath, targetName));
---
+++ lib.js/cli/commandline.js
@@ -0,0 +1,77 @@
+const clArgs = require('command-line-args');
+const clUsage = require('command-line-usage');
+
+const optionDefinitions = [
+  { 
+    name: 'in',
+    alias: 'i',
+    typeLabel: '{underline file}',
+    description: 'The input file to process.',
+    type: String,
+    group: 'main',
+  },
+  {
+    name: 'out',
+    alias: 'o',
+    type: String,
+    typeLabel: '{underline file}',
+    description: 'The target file.',
+    group: 'main',
+  },
+  {
+    name: 'verbose',
+    alias: 'v',
+    type: Boolean,
+    description: 'Generate verbose output.',
+  },
+  {
+    name: 'debug',
+    alias: 'd',
+    type: Boolean,
+    description: 'Generate debug output.',
+  },
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    description: 'Show help and exit.',
+    group: 'main',
+  },
+];
+
+const sections = [
+  {
+    header: 'converter command line interface',
+    content: 'Generate 8bit rgb png from 16bit raw or png files.'
+  },
+  {
+    header: 'Main options',
+    optionList: optionDefinitions,
+    group: [ 'main' ],
+  },
+  {
+    header: 'Misc',
+    optionList: optionDefinitions,
+    group: [ '_none' ],
+  },
+];
+
+const options = clArgs(optionDefinitions);
+
+if (options.main.help) {
+  console.log(clUsage(sections));
+  process.exit(0);
+};
+
+if (options._none.debug) {
+  process.env.DEBUG = true;
+};
+
+if (options._none.verbose) {
+  process.env.VERBOSE = true;
+};
+
+module.exports = {
+  getSource: () => options.main.in,
+  getTarget: () => options.main.out,
+};
